9e382a479cfc9855babd3fc8f6e8b9bc813967ac
Charlie Kun <157261888+wNv-paCe@users.noreply.github.com>
2024-10-28 18:49:51 -0600

    lab 3

--- components/app.js
+++ components/app.js
@@ -1,13 +1,16 @@
 import { View, Text } from "react-native";
+import { useState } from "react";
 import React from "react";
 import ToDoList from "./todolist";
 import ToDoForm from "./todoform";
 
 const App = () => {
+  const [tasks, setTasks] = useState(["Do laundry", "Go to gym", "Walk dog"]);
+
   return (
     <View>
-      <ToDoList />
-      <ToDoForm />
+      <ToDoList tasks={tasks} />
+      <ToDoForm setTasks={setTasks} />
     </View>
   );
 };
